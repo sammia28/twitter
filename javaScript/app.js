4079bfeede1bce8a18db601edbd49c8a0ab4107f
sammia28 <sammia.zubizarreta@gmail.com>
2016-09-19 19:05:10 -0500

    contador de caracteres

--- javaScript/app.js
+++ javaScript/app.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", function(){
     var boton = document.getElementById("publicar");
-    boton.addEventListener("click",function(){
+    boton.addEventListener("click",function(e){
+        e.preventDefault();
         var texto = document.getElementById("ingreso-texto").value ;
         var newContenido = document.createElement ("div");
         newContenido.innerHTML  = texto;
@@ -10,3 +11,16 @@ window.addEventListener("load", function(){
     });
 })
 
+function contador (){
+    var cantidadMax  = "140";
+    var contenido = ingreso-texto.length;
+    if( contenido < cantidadMax) {
+        
+    }else{
+        
+    }
+}
+
+
+var textoArea = document.getElementById("ingreso-texto").value;
+var numeroCarateres = 
