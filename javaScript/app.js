c265ac4a57918b9274dd734ade595c8361852c20
sammia28 <sammia.zubizarreta@gmail.com>
2016-09-22 10:13:04 -0500

    subiendo ultimos cambios

--- javaScript/app.js
+++ javaScript/app.js
@@ -13,15 +13,28 @@ window.addEventListener("load", function(){
         var publicacionesVarias = document.getElementById("tweet-publicado");
         publicacionesVarias.insertBefore(newContenido, publicacionesVarias.childNodes[0]);
         document.getElementById("ingresoTexto").value = "";
+        horaPublicacion(newContenido);
         contador.innerHTML = 140;
         boton.disable = true;
     });
-    ingresoTexto.addEventListener("keyup", function(){
+    function horaPublicacion (newContenido){
+        var horaActual = new Date ();
+        var hora = horaActual.getHours();
+        var minuto = horaActual.getMinutes();
+        var horaActual = hora + ":"+ minuto;
+        var horaContenedor = document.createElement("div");
+        horaContenedor.innerHTML = horaActual;
+        newContenido.insertBefore(horaContenedor, newContenido.childNodes[0]);
+    };
+    ingresoTexto.addEventListener("keypress", function(){
         boton.disabled = false;
         var cantidadCaracteres = texto.value.length;
         contador.innerHTML = caracterMax-cantidadCaracteres;
         if (cantidadCaracteres === 0 ){
             boton.disabled  = true;
+        }
+        if(cantidadCaracteres < 0){
+            boton.disabled = true;
         }
         if( cantidadCaracteres > 140){
             boton.disabled = true;
@@ -38,7 +51,7 @@ window.addEventListener("load", function(){
             contador.style.color = "red";
         }
     });
-    texto.addEventListener("keyup", function(){
+    texto.addEventListener("kepress", function(){
         if(window.event.keyCode == 13){
             if (this.value.split("\n").length != 0){
                 this.rows = this.value.split("\n").length+2
@@ -46,25 +59,14 @@ window.addEventListener("load", function(){
                 this.rows = 2;
             };
         };
-        /*var blablaContenido = document.getElementById("ingresoTexto");
-        blablaContenido.addEventListener("keyup",function(){
-            if( window.event)
-        });*/
-        
-        
-        
-        
-        
-        
-  /*      var textarea = document.getElementById("ingresoTexto");
-        textarea.addEventListener("input", autosize);
-        function autosize(){
-            textarea.style.cssText = "padding:0";
-            textarea.style.cssText = "height:0" + textarea.scrollHeight + "px";
-        }*/
-           
     }); 
+    texto.addEventListener("keypress", function(){
+        texto.style.cssText = "height:auto; padding: 0";
+        texto.style.cssText = "height : " + texto.scrollHeight + "px"; 
+     });
 })
+
+
 
 
 
